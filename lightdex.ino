8bc8a7ef9272392fdb54759f97ac5a68e24ec247
mjjoaquin <78010360+mjjoaquin@users.noreply.github.com>
2021-01-26 00:50:43 -0500

    Create lightdex.ino
    An incomplete, untested build. Can do simple animations and has modularity.

---
+++ lightdex.ino
@@ -0,0 +1,106 @@
+#include <Adafruit_NeoPixel.h>
+#define panelcount 4
+#define ledseach 12
+#define fadeSteps 10
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(panelcount * ledseach, 6, NEO_GRB + NEO_KHZ800);
+ 
+void setup() {
+  strip.begin();
+  Static(255, 255, 255);
+  Pulse(0, 0, 0, 255, 0, 0);
+}
+ 
+void loop() {
+  //Simple rainbow effect
+  FadeAll(255, 0, 0, 255, 255, 0);
+  FadeAll(255, 255, 0, 0, 255, 0);
+  FadeAll(0, 255, 0, 0, 255, 255);
+  FadeAll(0, 255, 255, 0, 0, 255);
+  FadeAll(0, 0, 255, 255, 0, 255);
+  FadeAll(255, 0, 255, 255, 0, 0);
+}
+
+//Sets panels individually. panelNumber starts at 0, from the arduino side of the strip
+void SetPanel(int panelNumber, int red, int green, int blue) {
+  for (int i = 0; i <= ledseach-1; i++) {
+  	strip.setPixelColor(i+12*panelNumber, red, green, blue);
+  }
+  strip.show();
+}
+
+//Sets everything to one colour. Good for clearing or no animation
+void Static(int red, int green, int blue) {
+  for (int i = 0; i <= panelcount*ledseach-1; i++) {
+  	strip.setPixelColor(i , red, green, blue);
+  }
+  strip.show();
+}
+
+//Pulses entire array from one colour to another and back
+//Takes (delayTime*fadeSteps*2) mills to run
+void Pulse(int red1, int green1, int blue1, int red2, int green2, int blue2) {
+  	int delayTime = 100;
+	for (int col = 0; col <= fadeSteps; col++) {
+		for (int i = 0; i <= panelcount*ledseach-1; i++) {
+			strip.setPixelColor(i, ((red2-red1)*col/fadeSteps)+red1, ((green2-green1)*col/fadeSteps)+green1, ((blue2-blue1)*col/fadeSteps)+blue1);
+        }
+      strip.show();
+      delay(delayTime);
+	}
+  	for (int col = 0; col <= fadeSteps; col++) {
+		for (int i = 0; i <= panelcount*ledseach-1; i++) {
+			strip.setPixelColor(i, ((red1-red2)*col/fadeSteps)+red2, ((green1-green2)*col/fadeSteps)+green2, ((blue1-blue2)*col/fadeSteps)+blue2);
+        }
+      strip.show();
+      delay(delayTime);
+	}
+}
+
+
+//Pulses one panel from one colour to another and back
+//Takes (delayTime*fadeSteps*2) mills to run
+void PulsePanel(int panelNumber, int red1, int green1, int blue1, int red2, int green2, int blue2) {
+  	int delayTime = 100;
+	for (int col = 0; col <= fadeSteps; col++) {
+		for (int i = panelNumber*12; i <= (panelNumber+1)*12-1; i++) {
+			strip.setPixelColor(i, ((red2-red1)*col/fadeSteps)+red1, ((green2-green1)*col/fadeSteps)+green1, ((blue2-blue1)*col/fadeSteps)+blue1);
+        }
+      strip.show();
+      delay(delayTime);
+	}
+  	for (int col = 0; col <= fadeSteps; col++) {
+		for (int i = panelNumber*12; i <= (panelNumber+1)*12-1; i++) {
+			strip.setPixelColor(i, ((red1-red2)*col/fadeSteps)+red2, ((green1-green2)*col/fadeSteps)+green2, ((blue1-blue2)*col/fadeSteps)+blue2);
+        }
+      strip.show();
+      delay(delayTime);
+	}
+}
+
+
+//Fades all panels at the same time from 1 to 2
+void FadeAll(int red1, int green1, int blue1, int red2, int green2, int blue2) {
+  for (int col = 0; col <= fadeSteps; col++) {
+      for (int i = 0; i <= panelcount*ledseach-1; i++) {
+        strip.setPixelColor(i, ((red2-red1)*col/fadeSteps)+red1, ((green2-green1)*col/fadeSteps)+green1, ((blue2-blue1)*col/fadeSteps)+blue1);
+      }
+      strip.show();
+      delay(100);
+    }
+}
+
+
+// legacy fade method
+//Fades each panel seperately from 1 to 2
+void Fade(int red1, int green1, int blue1, int red2, int green2, int blue2) {
+  for (int i = 0; i <= panelcount-1; i++) {
+    for (int col = 0; col <= fadeSteps-1; col++) { 
+      for (int j = 0; j <= ledseach-1; j++) {
+        strip.setPixelColor(i*ledseach+j , ((red2-red1)*col/fadeSteps)+red1, ((green2-green1)*col/fadeSteps)+green1, ((blue2-blue1)*col/fadeSteps)+blue1);
+      }
+      strip.show();
+      delay(100);
+    }
+  }
+}
