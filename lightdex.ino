4fd515de5fea242361ff516e669f87a3802d960a
mjjoaquin <78010360+mjjoaquin@users.noreply.github.com>
2021-02-02 18:18:51 -0500

    V1.1
    Added basic 2D functionality

--- lightdex.ino
+++ lightdex.ino
@@ -1,24 +1,31 @@
 #include <Adafruit_NeoPixel.h>
-#define panelcount 4
+#define panelcount 6
 #define ledseach 12
 #define fadeSteps 10
+
+//X and Y layout of panels in order they are wired
+//Refer to "2D diagram.png" for example
+int panelX[] = {3, 3, 2, 1, 0, 2};
+int panelY[] = {1, 0, 0, 0, 1, 1};
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(panelcount * ledseach, 6, NEO_GRB + NEO_KHZ800);
  
 void setup() {
   strip.begin();
-  Static(255, 255, 255);
-  Pulse(0, 0, 0, 255, 0, 0);
+  //Static(255, 255, 255);
+  //Pulse(0, 0, 0, 255, 0, 0);
+  SetPanelXY(2, 1, 255, 0, 255);
+  SetPanelXY(3, 0, 0, 255, 255);
 }
  
 void loop() {
-  //Simple rainbow effect
+  /*//Simple rainbow effect
   FadeAll(255, 0, 0, 255, 255, 0);
   FadeAll(255, 255, 0, 0, 255, 0);
   FadeAll(0, 255, 0, 0, 255, 255);
   FadeAll(0, 255, 255, 0, 0, 255);
   FadeAll(0, 0, 255, 255, 0, 255);
-  FadeAll(255, 0, 255, 255, 0, 0);
+  FadeAll(255, 0, 255, 255, 0, 0);*/
 }
 
 //Sets panels individually. panelNumber starts at 0, from the arduino side of the strip
@@ -27,6 +34,20 @@ void SetPanel(int panelNumber, int red, int green, int blue) {
   	strip.setPixelColor(i+12*panelNumber, red, green, blue);
   }
   strip.show();
+}
+
+//Sets panels individually based on X & Y values
+void SetPanelXY(int x, int y, int red, int green, int blue) {
+  //Seach for panel number with coresponding coordinates
+  int panelNumber = 1;
+  for(int i = 0; i <= panelcount; i++) {
+    if(panelX[i]==x && panelY[i] == y) {
+     	panelNumber = i;
+    	break;
+  	}
+  }
+  //Set
+  SetPanel(panelNumber, red, green, blue);
 }
 
 //Sets everything to one colour. Good for clearing or no animation
